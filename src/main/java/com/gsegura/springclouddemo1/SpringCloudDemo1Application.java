fe4349ab537ead13e33d0fc7c427ba3a24b5ab01
German Segura <german.andres.segura@gmail.com>
2021-12-02 15:06:54 -0500

    Initial commit

---
+++ src/main/java/com/gsegura/springclouddemo1/SpringCloudDemo1Application.java
@@ -0,0 +1,13 @@
+package com.gsegura.springclouddemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDemo1Application.class, args);
+    }
+
+}
