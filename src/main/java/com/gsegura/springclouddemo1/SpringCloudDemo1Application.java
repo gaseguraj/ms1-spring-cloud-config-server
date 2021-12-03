bc8082c71ff04e618f4d701dc3434dce77fd70c8
German Segura <german.andres.segura@gmail.com>
2021-12-02 22:28:48 -0500

    Small fixes

--- src/main/java/com/gsegura/springclouddemo1/SpringCloudDemo1Application.java
+++ src/main/java/com/gsegura/springclouddemo1/SpringCloudDemo1Application.java
@@ -2,6 +2,8 @@ package com.gsegura.springclouddemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 @EnableConfigServer
 @SpringBootApplication
 public class SpringCloudDemo1Application {
